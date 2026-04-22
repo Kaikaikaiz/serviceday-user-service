6e0dd248c1a9998161c45e3bdddd66cb622f9e36
Kaikaikaiz <yingxin0411@gmail.com>
2026-04-22 17:12:13 +0800

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -2,4 +2,29 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from django.db.models.signals import post_migrate
+        post_migrate.connect(_setup_groups_and_users, sender=self)
+
+
+def _setup_groups_and_users(sender, **kwargs):
+    # auto-creates Employee + Administrator groups after migrations
+    try:
+        from django.contrib.auth.models import User, Group
+
+        employee_group, _ = Group.objects.get_or_create(name="Employee")
+        admin_group, _    = Group.objects.get_or_create(name="Administrator")
+
+        for user in User.objects.filter(is_staff=True):
+            if not user.groups.filter(name="Administrator").exists():
+                user.groups.add(admin_group)
+
+        for user in User.objects.filter(is_staff=False):
+            if not user.groups.exists():
+                user.groups.add(employee_group)
+
+    except Exception:
+        pass
