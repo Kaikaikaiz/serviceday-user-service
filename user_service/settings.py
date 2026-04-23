47ad246f4aea2a97af30967a6b737fe77e6e3553
MrQin877 <kuanchinzhong@gmail.com>
2026-04-23 21:23:20 +0800

    Update settings.py

--- user_service/settings.py
+++ user_service/settings.py
@@ -15,9 +15,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # third party
     'rest_framework',
-    # local
+    'rest_framework_simplejwt.token_blacklist',
     'accounts',
 ]
 
