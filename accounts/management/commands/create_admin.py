2b1b778617448819a8999a2ee80eb409e5312d99
MrQin877 <kuanchinzhong@gmail.com>
2026-04-25 21:09:50 +0800

    fix command

--- accounts/management/commands/create_admin.py
+++ accounts/management/commands/create_admin.py
@@ -4,9 +4,13 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         User = get_user_model()
+
         if not User.objects.filter(username='svd_admin').exists():
             User.objects.create_superuser(
                 username='svd_admin',
-                email='admin@example.com',
-                password='admin123'
+                email='noreply.serviceday@gmail.com',
+                password='Svd1234*'
             )
+            self.stdout.write(self.style.SUCCESS("Admin created"))
+        else:
+            self.stdout.write("Admin already exists")
